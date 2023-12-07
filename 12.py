71ea9bec873cef0175ad36d6b6fb89bac2637290
kvshavlenko <kshv2006@mail.ru>
2023-12-07 14:29:06 +0700

    поле

--- 12.py
+++ 12.py
@@ -13,6 +13,7 @@ class Board:
         self.rects = []
 
     def render(self, screen):
+        global x_pos
         pygame.draw.line(screen, pygame.Color(0, 0, 255), [10, 10],
                          [10 + 17 * 30, 10], width=3)
         pygame.draw.line(screen, pygame.Color(0, 0, 255), [10, 10 + 17 * 30],
@@ -47,6 +48,8 @@ class Board:
 
 
 
+
+
         """for i in range(self.height):
             for j in range(self.width):
                 if self.board[i][j] == 255:
@@ -62,8 +65,7 @@ class Board:
     def checker(self, x, y):
         one = (x - self.top) // self.cell_size
         sec = (y - self.left) // self.cell_size
-        print(one, sec)
-        print(self.board[sec][one])
+        return self.board[int(sec)][int(one)]
 """        if one < self.width and sec < self.height and x > self.left and y > self.top:
             for i in range(self.height):
                 for j in range(self.width):
@@ -71,7 +73,52 @@ class Board:
                     yy = self.cell_size * i + self.top
                     if j == one and i == sec:
                         self.board[i][j] = 255 - self.board[i][j]"""
+class Character:
+    def __init__(self, file, speed):
+        self.file = file
+        self.speed = speed
 
+    def moving(self, destination):
+        pass
+
+    def death(self):
+        pass
+
+    def get_pos(self):
+
+        x, y = (0, 0)
+        return (x, y)
+
+    def draw(self, screen, x_pos, y_pos):
+        global flag
+        #if x_pos > 20 and x_pos < 17 * 30 and y_pos > 20 and y_pos < 17 * 30:
+        #    flag = True
+        pygame.draw.circle(screen, (255, 0, 0), (x_pos, y_pos), 10)
+        #else:
+        #    flag = False
+
+
+
+class Ghost(Character):
+    def __init__(self, file, speed):
+        super().__init__(file, speed)
+
+
+class Pacman(Character):
+    def __init__(self, file, speed):
+        super().__init__(file, speed)
+
+    def moving(self, destinations):
+        global x_pos
+        global y_pos
+        if destinations == "r" and x_pos < 17 * 30 and board.checker(x_pos + 0.25, y_pos) == "0":
+            x_pos += 0.25
+        elif destinations == "l" and x_pos > 20 and board.checker(x_pos - 0.25, y_pos) == "0":
+            x_pos -= 0.25
+        elif destinations == "u" and y_pos > 20 and board.checker(x_pos, y_pos - 0.25) == "0":
+            y_pos -= 0.25
+        elif destinations == "d" and y_pos < 17 * 30 and board.checker(x_pos, y_pos + 0.25) == "0":
+            y_pos += 0.25
 
 if __name__ == '__main__':
     pygame.init()
@@ -80,8 +127,13 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     board = Board(17, 17)
     running = True
+    x_pos = 300
+    y_pos = 300
+    pacmen = Pacman("хз", 2)
+    press = "r"
+    flag = True
+
     while running:
-        screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -89,6 +141,19 @@ if __name__ == '__main__':
                 k = event.pos
                 print(k)
                 board.checker(k[0], k[1])
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    press = "l"
+                if event.key == pygame.K_RIGHT:
+                    press = "r"
+                if event.key == pygame.K_UP:
+                    press = "u"
+                if event.key == pygame.K_DOWN:
+                    press = "d"
+        pacmen.moving(press)
+
+
         screen.fill((0, 0, 0))
         board.render(screen)
+        pacmen.draw(screen, x_pos, y_pos)
         pygame.display.flip()
