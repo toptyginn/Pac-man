20d12b903473ad309a552ef0b84e5fcb5d67547c
kvshavlenko <150430919+kvshavlenko@users.noreply.github.com>
2024-01-11 23:37:34 +0700

    Update 123456.py
    норм призраки + столкновение призраков и пакмена

--- 123456.py
+++ 123456.py
@@ -113,8 +113,10 @@ class Board(pygame.sprite.Sprite):
                     pygame.draw.circle(screen, pygame.Color(255, 215, 0), (10 + 30 * j + 15, 15 + 10 + 30 * i), 3)
 
     def eat_money(self, x, y):
+        #print((y - 10) // 30)
         x1 = (x - 10) // 30
         y1 = (y - 10) // 30
+        #print(len(sp_mone), y1)
         if sp_mone[int(y1)][int(x1)] == "1":  # !!!!!!!!!!!!!!!!!!!1
             sp_mone[int(y1)][int(x1)] = "0"
 
@@ -136,7 +138,7 @@ class Board(pygame.sprite.Sprite):
 
 
 class Character(pygame.sprite.Sprite):
-    def __init__(self, file, speed, lev):
+    def __init__(self, file, lev):
         super().__init__(player_group, all_sprites)
         self.change_x = 0
         self.change_y = 0
@@ -147,40 +149,49 @@ class Character(pygame.sprite.Sprite):
         self.rect.x, self.rect.y = start_pos_level[file][lev - 1]
         all_sprites.add(self)
         player_group.add(self)
-        self.speed = speed
         #self.mask = pygame.mask.from_surface(self.image)
     def moving(self, pazmer, destinations, boarddd, speed):
+        speed = 3
         if destinations == "r":
             self.rect.x += speed
-            for i in walls_group:
-                if pacmen.update(i):
-                    print("Пакмен столкнулся со стеной")
-                    self.rect.x -= speed
-                else:
-                    if self.rect.x + 10 >= pazmer * 30 + 10 and (pazmer // 2 - 1) * 30 + 10 < self.rect.y < (
-                            pazmer // 2) * 30 + 10:
-                        self.rect.x = 10
+            #print(40 + (pazmer * 30), self.rect.x)
+            if self.rect.x >= (pazmer * 30) - 15:
+                self.rect.x -= speed
+            if self.rect.x + 10 >= pazmer * 30 + 10 and (pazmer // 2 - 1) * 30 + 10 < self.rect.y < (
+                    pazmer // 2) * 30 + 10:
+                self.rect.x = 10
+            else:
+                for i in walls_group:
+                    if self.update(i):
+                        self.rect.x -= speed
         elif destinations == "l":
             self.rect.x -= speed
-            for i in walls_group:
-                if pacmen.update(i):
-                    print("Пакмен столкнулся со стеной")
-                    self.rect.x += speed
+            if self.rect.x <= 10:
+                self.rect.x += speed
             if self.rect.x - 10 <= 10 and (pazmer // 2 - 1) * 30 + 10 < self.rect.y < (pazmer // 2) * 30 + 10:
                 self.rect.x = pazmer * 30
+            else:
+                for i in walls_group:
+                    if self.update(i) :
+                        self.rect.x += speed
+
         elif destinations == "u":
             self.rect.y -= speed
-            for i in walls_group:
-                if pacmen.update(i):
-                    print("Пакмен столкнулся со стеной")
-                    self.rect.y += speed
+            if self.rect.y <= 10:
+                self.rect.y += speed
+            else:
+                for i in walls_group:
+                    if self.update(i):
+                        self.rect.y += speed
         elif destinations == "d":
             self.rect.y += speed
-            for i in walls_group:
-                if pacmen.update(i):
-                    print("Пакмен столкнулся со стеной")
-                    self.rect.y -= speed
-        #Board.eat_money(self, self.rect.x, self.rect.y)
+            if self.rect.y >= (pazmer * 30) - 15:
+                self.rect.y -= speed
+            else:
+                for i in walls_group:
+
+                    if self.update(i) :
+                        self.rect.y -= speed
 
         """def moving(self, pazmer, destinations, boarddd):
         if destinations == "r":
@@ -244,18 +255,24 @@ class Character(pygame.sprite.Sprite):
 
 
 class Pacman(Character):
-    def __init__(self, speed, lev):
-        super().__init__('pac', speed, lev)
+    def __init__(self, lev):
+        super().__init__('pac', lev)
         self.mask = pygame.mask.from_surface(self.image)
 
     def moving(self, pazmer, destinations, boarddd, speed):
         Character.moving(self, pazmer, destinations, boarddd, speed)
+        #print( self.rect.x,  self.rect.y)
         Board.eat_money(self, self.rect.x, self.rect.y)
 
+    def died(self, pac, ghos):
+        #print(player_group[0], player_group[1])
+        if pygame.sprite.collide_mask(pac, ghos):
+            return True
+        return False
 
 class Ghost(Character):
-    def __init__(self, speed, lev):
-        super().__init__('ghost_r', speed, lev)
+    def __init__(self, lev):
+        super().__init__('ghost_r', lev)
         self.mask = pygame.mask.from_surface(self.image)
 
 if __name__ == '__main__':
@@ -273,7 +290,7 @@ if __name__ == '__main__':
     # 'money': load_imaghe('ghost1.png')
     start_pos_level = {
         'pac': [(8 * 30 + 10, 10 * 30 + 10), (10 * 30 + 10, 12 * 30 + 10), (12 * 30 + 10, 12 * 30 + 10)],
-        'ghost_r': [(7 * 30 + 10, 8 * 30 + 10), (200, 200), (7 * 30 + 10, 8 * 30 + 10)]
+        'ghost_r': [(8 * 30 + 10, 7 * 30 + 10), (10 * 30 + 10, 9 * 30 + 10), (11 * 30 + 10, 9 * 30 + 10)]
     }
     tile_width = tile_height = 50
     player = None
@@ -316,10 +333,10 @@ if __name__ == '__main__':
         Board.render(Board, screen, razmer_screen)
         running = True
         press = "r"
-        pacmen = Pacman(1, level)
-        ghost = Ghost(0.5, level)
-
-
+        pacmen = Pacman(level)
+        ghost = Ghost(level)
+        a = random.choice(["u", "l", "r", "d"])
+        q = 0
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -354,11 +371,17 @@ if __name__ == '__main__':
             if level == 2:
                 pass
             # print(razmer_screen, "razmer_screen")
-            pacmen.moving(razmer_screen, press, BOARD, level)
-            a = random.choice(["u", "l", "r", "d"])
-            #print(a)
-            ghost.moving(razmer_screen, random.choice(["r", "l", "u", "l"]), BOARD, level)
+            pacmen.moving(razmer_screen, press, BOARD, 3)
+
+            if q % 10 == 0:
+                a = random.choice(["u", "l", "r", "d"])
+                #print()
+            ghost.moving(razmer_screen, a, BOARD, 3)
+            pc, gh = player_group
+            if pacmen.died(pc, gh):
+                print("STOOOOOOP")
             pygame.display.flip()
+            q += 1
         all_sprites.remove(pacmen)
         player_group.remove(pacmen)
 
